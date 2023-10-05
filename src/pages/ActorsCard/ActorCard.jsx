16bb3aa263c13932fd081afe8d045401111dee1b
mlewis2257 <mlang.lewis@gmail.com>
2023-10-04 22:38:34 -0700

    actors card

--- src/pages/ActorsCard/ActorCard.jsx
+++ src/pages/ActorsCard/ActorCard.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
 import React from "react";
 
-const ActorCard = () => {
-  return <div>ActorCard</div>;
+function randNum(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const ActorCard = ({ actor }) => {
+  let num = randNum(200, 500);
+  return (
+    <Link to={`/actors/${actor}`}>
+      <div
+        className="actorCard"
+        style={{
+          backgroundImage: `url(https://picsum.photos/${num}) no-repeat center center`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div>
+          <h1>{actor}</h1>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default ActorCard;
--- src/pages/ActorsListPage/ActorsListPage.jsx
+++ src/pages/ActorsListPage/ActorsListPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import ActorCard from "../ActorsCard/ActorCard";
 
-const ActorsListPage = () => {
-  return <div>ActorsListPage</div>;
+const ActorsListPage = ({ movies }) => {
+  let actorsArr = [];
+  movies.forEach((movie) => {
+    actorsArr = actorsArr.concat(movie.cast);
+  });
+  const actorsSet = new Set(actorsArr);
+  const uniqueActorArr = Array.from(actorsSet);
+  const actorListItem = uniqueActorArr.map((actor, idx) => {
+    <ActorCard actor={actor} index={idx} key={idx} />;
+  });
+  return <div className="actors-list">{actorListItem}</div>;
 };
 
 export default ActorsListPage;
